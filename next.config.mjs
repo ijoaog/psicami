67715cd2beaf0d5cf026ada530198d7baf8c67bc
ijoaog <jgkl_25@hotmail.com>
2025-07-03 12:41:35 -0300

    feat: ajuste next

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    optimizeCss: true,
+  },
   webpack(config) {
-    config.resolve.alias['@'] = resolve(__dirname, '.');
+    config.resolve.alias['@'] = resolve(__dirname);
     return config;
   },
 };
